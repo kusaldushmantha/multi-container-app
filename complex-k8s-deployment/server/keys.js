42a2adb34edac28bfb061eeea17545778a3b2f28
kusaldushmantha <kusald.14@cse.mrt.ac.lk>
2020-01-18 18:48:54 +0530

    k8s deployment files added

---
+++ complex-k8s-deployment/server/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT,
+  podName: process.env.POD_NAME,
+  podUUID: process.env.POD_UUID
+};
